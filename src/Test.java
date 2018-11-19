b141a3c034a901b7f4385755cd41144973824bb2
cluy0944 <luycathy259@gmail.com>
2018-11-19 14:44:42 -0500

    add .getignore file.

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main (String[] arg){
+        int[] arr = {58,61,37,91};
+        Sorts.printArr(arr);
+        System.out.println();
+
+        Sorts.ascendingSort(arr);
+        Sorts.printArr(arr);
+        System.out.println();
+    }
+}
